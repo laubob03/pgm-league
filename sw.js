c0db20d23e15fa93a0b3aa133f863932c7482bce
laubob03 <laubob03@users.noreply.github.com>
2026-04-24 02:49:01 +0800

    v1.0.213: Fix manifest.json (W3C format), version.json loader (fetch), sw.js cache paths, deprecation warning

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE_NAME = 'pgm-league-v212';
+const CACHE_NAME = 'pgm-league-v213';
 const urlsToCache = [
-  '/index.html',
-  '/manifest.json',
-  '/generated-images/A_beautiful_realistic_football_2026-04-02T19-41-49.png'
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
@@ -10,12 +9,10 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
-  // 立即激活新版本
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  // 删除旧缓存
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -27,7 +24,6 @@ self.addEventListener('activate', event => {
       );
     })
   );
-  // 立即控制所有页面
   self.clients.claim();
 });
 
@@ -35,7 +31,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        // 获取新版本后更新缓存
         if (response.status === 200) {
           const responseClone = response.clone();
           caches.open(CACHE_NAME).then(cache => {
@@ -45,7 +40,6 @@ self.addEventListener('fetch', event => {
         return response;
       })
       .catch(() => {
-        // 网络失败时使用缓存
         return caches.match(event.request);
       })
   );
