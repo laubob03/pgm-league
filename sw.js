d66ee48f06a724dd487741fa458c8467bb93752c
laubob03 <laubob03@users.noreply.github.com>
2026-06-01 00:11:35 +0800

    v1.0.378: hide transferred-out players from active roster, keep data for stats

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// v1.0.365: 缁堟瀬淇 锟?鏈€绠€ SW锛屼笉缂撳瓨浠讳綍涓滆タ锛屽畨瑁呭嵆娓呴櫎鎵€鏈夋棫缂撳瓨
-const CACHE_NAME = 'pgm-league-v377';
+const CACHE_NAME = 'pgm-league-v378';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
