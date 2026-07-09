430e8190fc9252d19dcebca07755f9ad8f630546
laubob03 <laubob03@users.noreply.github.com>
2026-07-09 23:09:05 +0800

    fix: 榜单页直接点杯赛按钮不合并12强季后赛数据 (v1.0.434)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// v1.0.433
+// v1.0.434
 const CACHE_NAME = 'pgm-league-v433';
-const REQUIRED_VERSION = '1.0.433';
+const REQUIRED_VERSION = '1.0.434';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
