57ce3ece1ce33a08f10a44f1d5c4b3efeca72db1
laubob03 <laubob03@users.noreply.github.com>
2026-07-13 20:39:32 +0800

    feat: 新增乙级全队TAB（乙东+乙西24队汇总）；63赛季球队经initData填充 v1.0.454

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v1.0.453
-const CACHE_NAME = 'pgm-league-v453';
+﻿// v1.0.453
+const CACHE_NAME = 'pgm-league-v454';
 const REQUIRED_VERSION = '1.0.453';
 
 self.addEventListener('install', event => {
