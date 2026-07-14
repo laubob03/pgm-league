8ebb41ac251b2c794ff5357f2681dc761703e0a4
laubob03 <laubob03@users.noreply.github.com>
2026-07-14 15:43:32 +0800

    v1.0.472: Leeds队徽改回富勒姆原图631(671推送遗漏未生效)

--- sw2.js
+++ sw2.js
@@ -1,6 +1,6 @@
-// v1.0.471
-const CACHE_NAME = 'pgm-league-v471';
-const REQUIRED_VERSION = '1.0.471';
+// v1.0.472
+const CACHE_NAME = 'pgm-league-v472';
+const REQUIRED_VERSION = '1.0.472';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
