d200dbb87554842a9f6037a15c8d6505daec108f
laubob03 <laubob03@users.noreply.github.com>
2026-07-15 23:51:46 +0800

    v1.0.477: FC Porto->FC Nurnberg 队名/队徽/球员全量替换

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// v1.0.476
-const CACHE_NAME = 'pgm-league-v476';
-const REQUIRED_VERSION = '1.0.476';
+// v1.0.477
+const CACHE_NAME = 'pgm-league-v477';
+const REQUIRED_VERSION = '1.0.477';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
