c8053915d7b7cf4b58ec97300eb868dbd66f0eb5
laubob03 <laubob03@users.noreply.github.com>
2026-07-16 23:20:01 +0800

    v1.0.479: fix MOTM team detection - best player always showing on away side

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// v1.0.478
-const CACHE_NAME = 'pgm-league-v478';
-const REQUIRED_VERSION = '1.0.478';
+// v1.0.479
+const CACHE_NAME = 'pgm-league-v479';
+const REQUIRED_VERSION = '1.0.479';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
