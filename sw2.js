d8d3e97ec782e0f68f5ed85dc8767cb1b2481f69
laubob03 <laubob03@users.noreply.github.com>
2026-07-17 20:56:26 +0800

    v1.0.486: fix player name click - undefined currentPlayerPos/currentPlayerCa ReferenceError in buildPlayerSeasonDetailHtml

--- sw2.js
+++ sw2.js
@@ -1,5 +1,5 @@
-// v1.0.485 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-league-v485';
+// v1.0.486 - cache name bumped to force old SW replacement
+const CACHE_NAME = 'pgm-league-v486';
 const REQUIRED_VERSION = '1.0.483';
 
 self.addEventListener('install', event => {
