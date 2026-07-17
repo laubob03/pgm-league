bf824faf3bcd9ee86c4bb9a7725bf31a1d5c5f01
laubob03 <laubob03@users.noreply.github.com>
2026-07-18 05:25:46 +0800

    v1.0.490: 63赛季甲级24队759名球员全量更新

--- sw2.js
+++ sw2.js
@@ -1,5 +1,5 @@
-// v1.0.489 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-league-v489';
+// v1.0.490 - cache name bumped to force old SW replacement
+const CACHE_NAME = 'pgm-league-v490';
 const REQUIRED_VERSION = '1.0.483';
 
 self.addEventListener('install', event => {
