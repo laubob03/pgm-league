2d1f06d1f2ea0610bae4357b616ecfbef0357b82
laubob03 <laubob03@users.noreply.github.com>
2026-07-23 21:46:32 +0800

    v1.0.507: Atalanta → Angers (jiaWest), 队徽875.png, TEAM_RENAME_MAP updated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿// v1.0.506 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-v1.0.506';
+﻿// v1.0.507 - cache name bumped to force old SW replacement
+const CACHE_NAME = 'pgm-v1.0.507';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
