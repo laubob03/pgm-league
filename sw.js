1beb48baeddd718477692f7a042072a26266b8cc
laubob03 <laubob03@users.noreply.github.com>
2026-07-25 00:54:22 +0800

    v1.0.517 杯赛CUP_FIXTURES中homeTeam/awayTeam/teams从数字改为字符串tX匹配state.teams[].id

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// v1.0.509 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-v1.0.516';
+const CACHE_NAME = 'pgm-v1.0.517';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
