942fa08b701a6e9ded26a0c36bed69577650fb19
laubob03 <laubob03@users.noreply.github.com>
2026-07-25 00:20:45 +0800

    v1.0.516 修复杯赛填充:云端cups缺championsCup等key时自动创建

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// v1.0.509 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-v1.0.515';
+const CACHE_NAME = 'pgm-v1.0.516';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
