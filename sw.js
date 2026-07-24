f26bef340b0fe9a7e4e6d469272c0b5dec75ead9
laubob03 <laubob03@users.noreply.github.com>
2026-07-25 00:15:05 +0800

    v1.0.515 添加debugCups()调试函数

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// v1.0.509 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-v1.0.514';
+const CACHE_NAME = 'pgm-v1.0.515';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
