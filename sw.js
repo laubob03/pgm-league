07a046314de5b380c3f3c8cb613c78f4b721e12f
laubob03 <laubob03@users.noreply.github.com>
2026-07-25 15:29:08 +0800

    v1.0.522: 63赛季五大联赛赛程导入(超级+甲乙东西各22轮)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// v1.0.509 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-v1.0.521';
+const CACHE_NAME = 'pgm-v1.0.522';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
