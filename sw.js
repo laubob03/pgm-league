47fef140132e759736785b3c8395c7db4c0378a0
laubob03 <laubob03@users.noreply.github.com>
2026-07-31 17:20:49 +0800

    v1.0.530: 致命修复-needUpdate时区bug(pgm_last_sync_time本地时间vs UTC差8h→永不下载)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// v1.0.509 - cache name bumped to force old SW replacement
-const CACHE_NAME = 'pgm-v1.0.529';
+const CACHE_NAME = 'pgm-v1.0.530';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
